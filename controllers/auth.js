37a3ec4ebeb0feb5d8cc2c7d8383da9f839c5373
akash mn <akashmnmdy@gmail.com>
2023-10-07 10:34:43 +0530

    login

--- controllers/auth.js
+++ controllers/auth.js
@@ -57,7 +57,8 @@ export const login = async (req, res) => {
     const token = jwt.sign({ id: user._id }, process.env.JWT_Secret, {
       expiresIn: 360000,
     });
-    res.status(200).json({ token, user });
+    const userWithoutPassword = { ...user._doc, password: undefined };
+    res.status(200).json({ token, user: userWithoutPassword });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
